4097e22d15669adc51acde7c94e1f0d6bb28d70e
Shamim Bhuiyan <bhuiyanshamim09@gmail.com>
2026-05-06 08:49:50 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const rattingBtn = document.querySelectorAll(".rating-btn");
+const subBtn = document.querySelector(".sub-btn");
+const container = document.querySelector(".container");
+const thankYou = document.getElementById("thank-you");
+const para = document.getElementById("para");
+
+let selectinedRatting = "";
+
+rattingBtn.forEach((btn)=>{
+  btn.addEventListener("click",()=>{
+    selectinedRatting = btn.textContent;
+  })
+});
+
+subBtn.addEventListener("click",()=>{
+  if(selectinedRatting === ""){
+    alert("Please select a rating first");
+    return;
+  }
+  container.style.display = "none";
+  thankYou.style.display = "block";
+
+  para.textContent = `You selected ${selectinedRatting} out of 5`
+});
